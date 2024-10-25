80e7b35d00c57a6e375a2a3b8b0d88ca2d104146
Damai <@damaiputrayudha1@gmail.com>
2024-10-25 15:53:43 +0700

    update pertama

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,27 @@
+import 'package:flame/flame.dart';
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
 import 'package:game_burung/game/flappy_bird_game.dart';
+import 'package:flutter/services.dart';
+import 'package:game_burung/screens/main_menu_screen.dart';
+import 'screens/game_over_screen.dart';
 
-void main() {
+Future<void> main() async {
+  SystemChrome.setSystemUIOverlayStyle(
+      const SystemUiOverlayStyle(statusBarColor: Colors.transparent));
+  WidgetsFlutterBinding.ensureInitialized();
+  await Flame.device.fullScreen();
+
   final game = FlappyBirdGame();
   runApp(
-    GameWidget(game: game),
+    GameWidget(
+      game: game,
+      initialActiveOverlays: const [MainMenuScreen.id],
+      overlayBuilderMap: {
+        'mainMenu': (context, _) => MainMenuScreen(game: game),
+        'gameOver': (context, _) => GameOverScreen(game: game),
+      },
+    ),
   );
 }
+
